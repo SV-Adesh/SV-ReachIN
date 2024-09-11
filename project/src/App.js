2a59f79e9182284c6a94d32f0a1d1453febf319d
adesh <adeshshetty3333@gmail.com>
2024-09-11 18:55:33 +0530

    First  Commit

---
+++ project/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginPage from './components/Login'; // Ensure this path is correct
+
+const App = () => (
+  <div>
+    <LoginPage />
+  </div>
+);
+
+export default App; // Ensure App is the default export
